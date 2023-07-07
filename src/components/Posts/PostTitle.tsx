d923e08e396c40349c82a4d22849d23ae5f40d34
김진평 <jp@gimjinpyeong-ui-MacBookPro.local>
2023-07-07 09:15:15 +0900

    ADD : 블로그 프로젝트 회고 포스트 추가

--- src/components/Posts/PostTitle.tsx
+++ src/components/Posts/PostTitle.tsx
@@ -8,9 +8,10 @@ type Props = {
 const PostTitle = ({ post }: Props) => {
   return (
     <section className="text-center">
-      <h1 className="pt-16 text-4xl font-bold">{post.title}</h1>
-      <h2 className="text-2xlp pb-8 border-b-[0.5px] border-gray-400">
-        {post.category}
+      <h1 className="pt-16 text-4xl font-bold pb-2">{post.title}</h1>
+      <h2 className="pb-2 text-md">{post.description}</h2>
+      <h2 className="pb-8 text-xs border-b-[0.5px] border-gray-400">
+        {`[${post.category}]`}
       </h2>
     </section>
   );
